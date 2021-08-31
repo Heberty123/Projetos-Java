868c585051134aedd248c9f295ff75986df3ddaa
Perkles <hebertyprado22005@gmail.com>
2021-08-31 06:13:25 -0300

    Eu apenas simplifiquei o codigo, instanciação dentro da lista.

--- exercicio8/src/application/Program.java
+++ exercicio8/src/application/Program.java
@@ -41,22 +41,20 @@ public class Program {
             System.out.print("Enter Salario of employee "+ (i + 1)+ " : ");
             double salario = sc.nextDouble();
             
-            Employee emp = new Employee(id, name, salario);
-            
             list.add(new Employee(id, name, salario));
             
         }
         
         System.out.println("Enter the employee id that will have salary increase : ");
         int id = sc.nextInt(); 
-        Employee emp = list.stream().filter(x -> x.setId() == id).findFirst().orElse(null);
-		if (emp == null) {
+        Employee empp = list.stream().filter(x -> x.setId() == id).findFirst().orElse(null);
+		if (empp == null) {
 			System.out.println("This id does not exist!");
 		}
 		else {
 			System.out.print("Enter the percentage: ");
 			double percentage = sc.nextDouble();
-			emp.IncreaseSalary(percentage);
+			empp.IncreaseSalary(percentage);
 		}
 		
 		System.out.println();
