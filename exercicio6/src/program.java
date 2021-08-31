efc2415e3b1938e1cd0bb368ecf04a3088d1808b
Heberty <82103610+Heberty123@users.noreply.github.com>
2021-08-31 08:00:01 -0300

    Update program.java
    Adicionei comentário e também um variável para ler o nome de usuário e imprimi-lo no programa.

--- exercicio6/src/program.java
+++ exercicio6/src/program.java
@@ -7,9 +7,14 @@ public static void main(String[] args) {
     
 Locale.setDefault(Locale.US);
 Scanner sc = new Scanner(System.in);
+    
+ System.out.println()
+ System.out.print("Bem vindo ! Porfavor digite seu nome : ");
+ sc.nextLine();
+ String nameUsu = sc.nextLine();
 
 System.out.println("Enter product data: ");
-System.out.print("Name: ");
+System.out.print("olá " + nameUsu + ", digite o Name do produto:");
 String name = sc.nextLine();
 System.out.print("Price: ");
 double price = sc.nextDouble();
@@ -17,7 +22,7 @@ double price = sc.nextDouble();
 Product product = new Product(name, price);
 
 System.out.println();
-System.out.println("Product data: " + product);
+System.out.println("Product data: " + product); //Aqui coloquei product, pois java automaticamente vai procurar dentro dessa classe, o método toString.
 System.out.println();
 System.out.print("Enter the number of products to be added in stock: ");
 int quantity = sc.nextInt();
