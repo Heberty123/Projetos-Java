a570cf0b0606c87208ad4a9d0e6e061639231a29
Heberty <hebertyprado22005@gmail.com>
2021-11-13 18:17:22 -0300

    att

---
+++ Voluntario30_javaIO/src/InpOut/CreateFile.java
@@ -0,0 +1,54 @@
+package InpOut;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
+public class CreateFile {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		FileInputStream fis = new FileInputStream("file1.txt");
+		Reader r = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(r);
+		
+		String linha = br.readLine();
+		
+		
+	//	FileOutputStream fos = new FileOutputStream("file2.txt");
+	//	OutputStreamWriter ro = new OutputStreamWriter(fos);
+	//	BufferedWriter bw = new BufferedWriter(ro);
+		
+	//	while (linha != null) {	
+	//		System.out.println(linha);
+	//		bw.write(linha);
+	//		bw.newLine();
+	//		linha = br.readLine();
+	//	}
+		
+		
+		PrintStream bw = new PrintStream("file2.txt");
+	//	PrintWriter bw = new PrintWriter(new File("file2.txt"));
+		
+		while (linha != null) {	
+			System.out.println(linha);
+			bw.println(linha);
+			linha = br.readLine();
+		}
+		
+		
+	}
+
+}
